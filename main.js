8f5cdb5dcc64ba1c39481ea3b2a824fe2a563760
prinsi-t <tprincy56@gmail.com>
2024-05-29 20:46:48 +0530

    added some responsive

--- main.js
+++ main.js
@@ -54,3 +54,9 @@ function myFunction() {
     let element = document.body;
     element.classList.toggle("dark");
 }
+
+/////hamburger menu
+document.querySelector('.hamburger-button').addEventListener('click', function() {
+    document.querySelector('.nav-links').style.display = 
+    (document.querySelector('.nav-links').style.display == 'none') ? 'block' : 'none';
+ });
